679cdf1a9c0660af74982ebb39161778f22c6af5
Angie4x <angiel.aldanac@outlook.com>
2022-09-09 22:19:00 -0500

    okApi

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+
+
+#!/usr/bin/python3
+""" Python script that, using this REST API,
+for a given employee ID, returns information about his/her
+TODO list progress."""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    userId = (argv[1])
+    respUser = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(userId))
+    respTodos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos".format(userId))
+
+    taskcompleted = 0
+    totalnumtask = 0
+    name = respUser.json().get('name')
+    Title = []
+
+    for item in respTodos.json():
+        if item.get('userId') == int(userId):
+            totalnumtask += 1
+            if item.get('completed'):
+                taskcompleted += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(name,
+          taskcompleted, totalnumtask))
+
+    print('\n'.join(["\t " + item.get('title') for item in respTodos.json()
+          if item.get('userId') == int(userId) and item.get('completed')]))
+          
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python script to export data in the CSV format"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    userId = (argv[1])
+    respUser = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(userId))
+    respTodos = requests.get(
+        "https://jsonplaceholder.typicode.com/todos".format(userId))
+
+    name = respUser.json().get('username')
+
+    with open('{}.csv'.format(userId), 'w', newline='') as csvfile:
+        Cwrite = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for item in respTodos.json():
+            if item.get('userId') == int(userId):
+                Cwrite.writerow(
+                    [userId, name, item.get('completed'), item.get('title')])
+                    
